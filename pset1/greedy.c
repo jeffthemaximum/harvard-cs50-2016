0ae59e25a9577e0f756ccf98479f7aafc6748e1d
jeff maxim <jeff@experiment.com>
2016-09-29 21:15:25 +0000

    solves greedy

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -1,2 +1,52 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
+
+#define QUARTER 25
+#define DIME 10
+#define NICKLE 5
+#define PENNY 01
+
+int main(void) {
+    
+    // greet user
+    printf("O hai! ");
+    
+    // get input
+    float input;
+    do {
+        printf("How much change is owed?\n");
+        input = get_float();
+    } while (input < 0);
+    
+    // convert money to int
+    input = input * 100;
+    int money = round(input);
+
+    // instantiate coins count
+    int c_count = 0;
+    
+    // quarters
+    if (money >= QUARTER) {
+        c_count += (money / QUARTER);
+        money = money % QUARTER;
+    }
+    
+    // dimes
+    if (money >= DIME) {
+        c_count += (money / DIME);
+        money = money % DIME;
+    }
+    
+    // nickles
+    if (money >= NICKLE) {
+        c_count += (money / NICKLE);
+        money = money % NICKLE;
+    }
+    
+    // pennies
+    c_count += money;
+    
+    printf("%i\n", c_count);
+}
+
